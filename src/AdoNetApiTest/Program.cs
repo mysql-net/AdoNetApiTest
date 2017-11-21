cae46e8d6d0f377d513341770a253eac393b5813
Bradley Grainger <bgrainger@gmail.com>
2017-11-20 22:46:06 -0800

    Add simple HTML output dump.

--- src/AdoNetApiTest/Program.cs
+++ src/AdoNetApiTest/Program.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using AdoNetApiTest.Connectors;
 using AdoNetApiTest.Tests;
 using MySqlConnector = AdoNetApiTest.Connectors.MySqlConnector;
@@ -10,14 +13,47 @@ namespace AdoNetApiTest
 {
 	class Program
 	{
+		static List<string> s_rows;
+
 		static void Main(string[] args)
 		{
 			var connectors = CreateConnectors();
+
+			var sb = new StringBuilder(@"<!DOCTYPE html>
+<HTML>
+<HEAD>
+<TITLE>ADO.NET API Tests</TITLE>
+<LINK rel='stylesheet' type='text/css' href='http://seriot.ch/json/style.css'>
+<META charset='utf-8'>
+</HEAD>
+
+<BODY>
+<H1>ADO.NET API Tests</H1>
+<TABLE>
+    <TR>
+        <TH></TH>
+");
+			foreach (var connector in connectors)
+				sb.AppendFormat("<TH class='vertical'><DIV>{0}</DIV></TH>", connector.Name);
+			sb.AppendLine("</TR>");
+
+			s_rows = new List<string>();
+
 			foreach (var connector in connectors)
 				RunAllTests(connector);
 
 			foreach (var connector in connectors)
 				connector.Uninitialize();
+
+			foreach (var row in s_rows)
+				sb.AppendLine($"<TR>{row}</TR>");
+			sb.Append(@"</TABLE>
+</BODY>
+</HTML>
+");
+			var path = Path.Combine(Path.GetTempPath(), "results.html");
+			File.WriteAllText(path, sb.ToString());
+			Process.Start(path);
 		}
 
 		private static IReadOnlyList<Connector> CreateConnectors()
@@ -50,6 +86,8 @@ namespace AdoNetApiTest
 
 		private static void RunAllTests(Connector connector)
 		{
+			int index = 0;
+
 			Console.WriteLine(connector.Name);
 			Console.WriteLine(new string('=', connector.Name.Length));
 			var testTypes = Assembly.GetExecutingAssembly().GetTypes().Where(x => x.IsSubclassOf(typeof(TestBase))).ToList();
@@ -63,19 +101,28 @@ namespace AdoNetApiTest
 				foreach (var method in testType.GetMethods(BindingFlags.Instance | BindingFlags.Public).Where(x => x.ReturnType == typeof(bool) && x.GetParameters().Length == 0))
 				{
 					Console.Write("{0}: ", method.Name);
+					if (index >= s_rows.Count)
+						s_rows.Add($"<TD>{method.Name}</TD>");
+
 					try
 					{
 						var result = (bool) method.Invoke(test, new object[0]);
 						Console.WriteLine(result);
+						var className = result ? "EXPECTED_RESULT" : "SHOULD_HAVE_PASSED";
+						s_rows[index] += $"<TD class='{className}'></TD>";
 					}
 					catch (TargetInvocationException ex)
 					{
+						s_rows[index] += $"<TD class='CRASH'></TD>";
 						Console.WriteLine("EXCEPTION {0} {1}", ex.InnerException.GetType().Name, ex.InnerException.Message);
 					}
 					catch (Exception ex)
 					{
+						s_rows[index] += $"<TD class='CRASH'></TD>";
 						Console.WriteLine("UNEXPECTED EXCEPTION {0} {1}", ex.GetType().Name, ex.Message);
 					}
+
+					index++;
 				}
 			}
 			Console.WriteLine();
