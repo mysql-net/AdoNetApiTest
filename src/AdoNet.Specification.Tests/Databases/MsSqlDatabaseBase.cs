f01a32b6591a4afcb7ec0a51bf48ee40501b743b
Bradley Grainger <bgrainger@gmail.com>
2017-11-23 15:45:28 -0800

    Fix T-SQL Boolean literals.

--- src/AdoNet.Specification.Tests/Databases/MsSqlDatabaseBase.cs
+++ src/AdoNet.Specification.Tests/Databases/MsSqlDatabaseBase.cs
@@ -6,7 +6,7 @@ namespace AdoNet.Specification.Tests.Databases
 	{
 		// docker run --name mssql -e ACCEPT_EULA=Y -e MSSQL_SA_PASSWORD=Pa$$word -p 1433:1433 -d microsoft/mssql-server-linux:2017-latest
 		public override string ConnectionString => "data source=localhost;user id=sa;password=Pa$$word";
-		public override string CreateBooleanLiteral(bool value) => value ? "1" : "0";
+		public override string CreateBooleanLiteral(bool value) => $"cast({(value ? 1: 0)} as bit)";
 		public override string CreateHexLiteral(byte[] value) => $"0x{BitConverter.ToString(value).Replace("-", "")}";
 	}
 }
