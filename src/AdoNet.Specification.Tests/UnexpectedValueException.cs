df83eb3c7549c0987b41e1a3997a2a59d1ae8025
Bradley Grainger <bgrainger@gmail.com>
2017-12-31 20:12:40 -0800

    Show contents of unexpected byte array.

--- src/AdoNet.Specification.Tests/UnexpectedValueException.cs
+++ src/AdoNet.Specification.Tests/UnexpectedValueException.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Data.Common;
+using System.Linq;
 
 namespace AdoNet.Specification.Tests
 {
@@ -10,8 +11,15 @@ namespace AdoNet.Specification.Tests
 	public class UnexpectedValueException : Exception
 	{
 		public UnexpectedValueException(object value)
-			: base(value == null ? "Unexpected value: (null)" : $"Unexpected value: {value} ({value.GetType()})")
+			: base(value == null ? "Unexpected value: (null)" : $"Unexpected value: {ToString(value)} ({value.GetType()})")
 		{
+		}
+
+		private static string ToString(object value)
+		{
+			if (value is byte[] byteArray)
+				return string.Join(" ", byteArray.Take(10).Select(x => x.ToString("X2"))) + (byteArray.Length > 10 ? "..." : "");
+			return value.ToString();
 		}
 	}
 }
