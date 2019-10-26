7d92273a0c8d7576fa40d3b066ba7c83bd90c960
Bradley Grainger <bgrainger@gmail.com>
2019-10-26 10:18:12 -0700

    Test CommandBehavior.SingleRow.

--- src/AdoNet.Specification.Tests/DataReaderTestBase.cs
+++ src/AdoNet.Specification.Tests/DataReaderTestBase.cs
@@ -576,6 +576,38 @@ namespace AdoNet.Specification.Tests
 		}
 
 		[Fact]
+		public virtual void SingleRow_returns_one_row()
+		{
+			using var connection = CreateOpenConnection();
+			using var command = connection.CreateCommand();
+			command.CommandText = "SELECT 1 UNION SELECT 2;";
+
+			using var reader = command.ExecuteReader(CommandBehavior.SingleRow);
+			var hasData = reader.Read();
+			Assert.True(hasData);
+			Assert.Equal(1L, Convert.ToInt64(reader.GetValue(0)));
+			Assert.False(reader.Read());
+
+			Assert.False(reader.NextResult());
+		}
+
+		[Fact]
+		public virtual void SingleRow_returns_one_result_set()
+		{
+			using var connection = CreateOpenConnection();
+			using var command = connection.CreateCommand();
+			command.CommandText = "SELECT 1; SELECT 2; SELECT 3;";
+
+			using var reader = command.ExecuteReader(CommandBehavior.SingleRow);
+			var hasData = reader.Read();
+			Assert.True(hasData);
+			Assert.Equal(1L, Convert.ToInt64(reader.GetValue(0)));
+			Assert.False(reader.Read());
+
+			Assert.False(reader.NextResult());
+		}
+
+		[Fact]
 		public virtual void NextResult_can_be_called_more_than_once()
 		{
 			using (var connection = CreateOpenConnection())
