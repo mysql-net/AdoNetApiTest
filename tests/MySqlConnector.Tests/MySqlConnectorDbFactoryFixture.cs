cd5846e7219af8c54a619f7fc5e44502fe411c7c
Bradley Grainger <bgrainger@gmail.com>
2020-06-26 12:30:45 -0700

    Update MySqlConnector to 1.0.0-beta.4.

--- tests/MySqlConnector.Tests/MySqlConnectorDbFactoryFixture.cs
+++ tests/MySqlConnector.Tests/MySqlConnectorDbFactoryFixture.cs
@@ -6,7 +6,7 @@ namespace MySqlConnector.Tests
 {
 	public class MySqlConnectorDbFactoryFixture : IDbFactoryFixture
 	{
-		public DbProviderFactory Factory => MySqlClientFactory.Instance;
+		public DbProviderFactory Factory => MySqlConnectorFactory.Instance;
 		public string ConnectionString => "Server=localhost;User Id=root;Password=test;SSL Mode=none;Database=mysqlconnector";
 	}
 }
